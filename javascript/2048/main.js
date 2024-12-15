7f5eb70101941f3d3af9530ff1491c5f1ecfc925
arnaud <arnaud@arnaud.com>
2024-12-15 14:49:32 +0100

    [JavaScript][2048] init

---
+++ javascript/2048/main.js
@@ -0,0 +1,19 @@
+console.log('coucou');
+
+// Generate a 2048 board 
+const generateBoard = (width, height) => {
+    const result = [];
+    for (let i=0; i<height; i++) {
+        const row = [];
+        for (let j=0; j<width; i++) {
+            row.push()
+        }        
+    }
+}
+
+const getRandomFiboNumber = () => {
+    const firstFiboSequence = [1, 2, 3, 5, 8, 13];
+    return firstFiboSequence[Math.floor(firstFiboSequence.length)];
+}
+
+console.log(getRandomFiboNumber());
---
+++ javascript/2048/main.spec.js
@@ -0,0 +1,10 @@
+describe('2048 Game', () => {
+  describe('test suite', () => {
+    test('first test', () => {
+      const a = 1;
+      const b = 2;
+      expect(a < 2).toBe(true);
+    });
+  });
+
+});
